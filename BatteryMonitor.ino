301478bc3fda56ea9f3b1625fdb38eec5b441d8c
Rami Mounla <Rami_Mounla@hotmail.com>
2020-05-15 17:20:26 +1200

    Added wifi connection

--- BatteryMonitor.ino
+++ BatteryMonitor.ino
@@ -1,9 +1,46 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <PubSubClient.h>
+
+#ifndef STASSID
+#define STASSID ""
+#define STAPSK  ""
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 void setup() {
   // put your setup code here, to run once:
+    pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, 1);
+  Serial.begin(115200);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  WiFi.hostname("ledhost"); //set hostname
+  
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  client.loop();
 }

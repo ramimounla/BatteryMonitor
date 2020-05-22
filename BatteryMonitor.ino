e5df3a05d90e8692db8a0631d3430052eebdee03
Rami Mounla <Rami_Mounla@hotmail.com>
2020-05-22 22:55:19 +1200

    Added webhook to write to IoT hub

--- BatteryMonitor.ino
+++ BatteryMonitor.ino
@@ -1,18 +1,22 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
 
 #ifndef STASSID
-#define STASSID "Maouzeh"
-#define STAPSK  "HighStreet5771952"
+#define STASSID ""
+#define STAPSK  ""
+#define WRITEKEY  ""
 #endif
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
+const String writeKey = WRITEKEY;
 
 float analogValue;
 float inputVoltage;
 
 WiFiClient espClient;
+HTTPClient http;
 
 void setup() {
   // put your setup code here, to run once:
@@ -43,16 +47,44 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Reading voltage");
+  Serial.println("Reading");
   
-  analogValue = analogRead(A0);
-  inputVoltage = analogValue * (3.3 / 1023.00);
+  analogValue = 0;
+
+  for(int i = 0 ; i < 10 ; i++){
+    analogValue =  analogValue + analogRead(A0);
+    delay(100); 
+  }
+  inputVoltage = (analogValue / 10) * (4.7 / 1023.00);
+
+  Serial.print(analogValue/10);
+  Serial.println(" Read");
+  
   Serial.print(inputVoltage);
   Serial.println("V");
+
+  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+  client->setInsecure();
+
+  String apiUrl = "https://api.thingspeak.com/update.json?api_key=" + writeKey + "&field3=" + inputVoltage;
   
+  HTTPClient http;    //Declare object of class HTTPClient
+  http.begin(*client, apiUrl);     //Specify request destination
+
+  Serial.println("Request:");
+  Serial.println(apiUrl);
+              
+  Serial.println("Sending request");
+  int httpCode = http.GET();
+
+  Serial.print("Resutl is: ");
+  Serial.println(httpCode);
+
+  http.end();  //Close connection
+
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
   delay(500);                      // Wait for a second
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(500);  
+  delay(300000);  
 
 }
